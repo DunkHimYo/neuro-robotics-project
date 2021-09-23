3b91edcdff33c20e4fbd39f446581a5b3c7a7739
DunkHimYo <57661649+DunkHimYo@users.noreply.github.com>
2021-09-24 06:14:37 +0900

    Create preprocessing_v2.py

---
+++ preprocessing_v2.py
@@ -0,0 +1,24 @@
+import mne
+from meegkit import star
+from meegkit.asr import ASR
+from meegkit.utils.matrix import sliding_window
+import numpy as np
+
+def preprocessing(frame,sfreq):
+    data = mne.evoked.detrend(frame)
+    data = mne.filter.filter_data(data=data, l_freq=4, h_freq=50, sfreq=128,verbose=False)
+    x = data.T
+    y, w, _ = star.star(x,verbose=False)
+
+    raw = y.T
+    asr = ASR(method='euclid')
+
+    train_idx = np.arange(0 * sfreq, 10 * sfreq, dtype=int)
+    _, sample_mask = asr.fit(raw[:, train_idx])
+    X = sliding_window(raw, window=int(sfreq), step=int(sfreq))
+    Y = np.zeros_like(X)
+    for i in range(X.shape[1]):
+        Y[:, i, :] = asr.transform(X[:, i, :])
+    Y=Y.transpose(2,1,0)
+
+    return Y
